40941541280458997f478fd8281de3e1532fc24c
Aldar <aldareev@gmail.com>
2021-02-11 11:35:28 +0300

    Чекпоинт 5 из 8

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const filterByAge = (el) => el.age < 50;
+
+const filterByScore = (el) => el.score > 75;
+
+const filterByServiceRecord = (el) => el.serviceRecord > 5;
+
+const sortByScore = (el) => el.score;
+
+const getMeanAge = (el) => {};
+
+const getNamesOnly = (el) => el.name;
+
+
 
 module.exports = {
   filterByAge,
