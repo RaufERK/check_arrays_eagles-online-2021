92a48bc871a83b09b65137c9d1d61ec491fdec8a
Artur <avrezn@gmail.com>
2021-02-11 10:19:34 +0300

    запутался, не получилось

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
+let candidates = [
+  {
+    name: 'Василий Петров',
+    age: 60,
+    score: 98,
+    serviceRecord: 36,
+  },
+  {
+    name: 'Екатерина Фёдорова',
+    age: 35,
+    score: 90,
+    serviceRecord: 7,
+  },
+  {
+    name: 'Иван Иванов',
+    age: 29,
+    score: 67,
+    serviceRecord: 2,
+  },
+  {
+    name: 'Зинаида Абрамова',
+    age: 45,
+    score: 78,
+    serviceRecord: 14,
+  },
+  {
+    name: 'Виктор Олегов',
+    age: 54,
+    score: 85,
+    serviceRecord: 21,
+  },
+];
+function filterByAge() {
+  let arr = candidates.filter(el => el.age < 50);
+  return arr;
+}
+ 
+function filterByScore() {
+  return candidates.filter((el) => el.score >= 75);
+}
+function filterByServiceRecord() {
+  return candidates.filter((el) => el > 5);
+}
+function sortByScore() {
+  
+}
+function getMeanAge() {
+
+}
+function getNamesOnly() {
+
+}
+console.log(filterByAge());
 
 module.exports = {
   filterByAge,
