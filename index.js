c5971ed2b724952ebe88b274cef927f728d39a7d
Andrei <ad933@mail.ru>
2021-02-11 11:16:49 +0400

    4 of 8

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const filterByAge = (el) => el.age < 50;
+
+const filterByScore = (el) => el.score > 75;
+
+const filterByServiceRecord = (el) => el.serviceRecord > 5;
+
+const sortByScore = (el) => el.sort((a, b) => b.score - a.score);
+
+const getMeanAge = (el) => el.reduce((acc, cur) => acc.age + cur.age)/el.length;
+
+const getNamesOnly = (candidates) => candidates.map((el) => el.name);
 
 module.exports = {
   filterByAge,
