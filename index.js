2c189fd199b89ef504301e5c91de61870ee02928
Oksana Klymenok <kapris15@yahoo.com>
2021-02-10 21:17:49 -1000

    Release0

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const filterByAge=(el)=>{return el.age<=50};
+const filterByScore=(el)=>el.score>75;
+  const filterByServiceRecord=(el)=>el.serviceRecord>5;
+  const sortByScore=(arr)=>{return arr.slice().sort((a,b)=>b.score-a.score)};
+
+  
+  function getMeanAge(arr){
+    let total=0;
+    arr.forEach(el=> total+=el.age);
+    return total/arr.length;
+  };
+  const getNamesOnly=()=>{}
+
+
+
+
 
 module.exports = {
   filterByAge,
@@ -7,3 +23,6 @@ module.exports = {
   getMeanAge,
   getNamesOnly,
 };
+
+
+
