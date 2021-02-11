8219411d90076066cff878d7531337283f707cd3
Dima <shetkodv@gmail.com>
2021-02-11 09:51:54 +0300

    Check-array

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+function filterByAge(el) {
+  return el.age < 50;
+}
+
+function filterByScore(el) {
+  return el.score > 75;
+}
+
+function filterByServiceRecord(el) {
+  return el.serviceRecord > 5;
+}
+
+function sortByScore(arr) {
+  const newArr = arr.slice();
+  return newArr.sort((a, b) => b.score - a.score);
+}
+
+function getMeanAge(arr) {
+  return arr.reduce((sum, el) => sum + el.age, 0) / arr.length;
+}
+
+function getNamesOnly(el) {
+  return el.name;
+}
 
 module.exports = {
   filterByAge,
